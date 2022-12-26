f1a7d36105523035a60ae0cd302ceec42db09f9b
linlin <denglinlin315@gmail.com>
2022-12-26 10:20:33 +1000

    update2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // UseStates variables 
+  const [audioProgress, setAudioProgress] = useState(60)
+
+  const handleMusicProgressBar = (e) => {
+    setAudioProgress(e.target.value)
+  }
+
   return (
     <>
     <div className="container">
-      <video src=""></video>
+      <video src="./Assets/Videos/Video1.mp4" autoPlay muted loop></video>
+      <div className="blackScreen"></div>
+      <div className="music-Container">
+        <p className="musicPlayer">Music Player</p>
+        <p className="music-Head-Name">Chasing</p>
+        <p className="music-Artist-Name">NEFFEX</p>
+        <img src="./Assets/Images/image1.jpg" alt="song Avatar" id="songAvatar" />
+        <div className="musicTimerDiv">
+          <p className="musicCurrentTime">00 : 00</p>
+          <p className="musicTotalLength">03 : 49</p>
+        </div>
+        <input type="range" name="musicProgressBar" className="musicProgressBar" value={audioProgress}
+        onChange={handleMusicProgressBar} />
+        <div className="musicControlers">
+          <i className='fa-solid fa-backward'></i>
+          <i className='fa-solid fa-cirdle-play'></i>
+          <i className='fa-solid fa-forward'></i>
+        </div>
+      </div>
     </div>
     </>
   );
