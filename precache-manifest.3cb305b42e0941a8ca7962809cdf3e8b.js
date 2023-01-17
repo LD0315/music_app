705f575eaf3e31b00454a8ca9ae7212885147f6a
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-17 11:48:50 +1000

    Updates

---
+++ precache-manifest.3cb305b42e0941a8ca7962809cdf3e8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "313dc64149346125276c",
+    "url": "/music_player/static/css/main.5fe87ede.chunk.css"
+  },
+  {
+    "revision": "313dc64149346125276c",
+    "url": "/music_player/static/js/main.313dc641.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/music_player/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "8333c4a731714592e30e",
+    "url": "/music_player/static/js/runtime~main.8333c4a7.js"
+  },
+  {
+    "revision": "32f07f4be9a83e89115863ca63240403",
+    "url": "/music_player/index.html"
+  }
+];
